32819345a33a963f8ad6d29843be8f0477984f2f
Manish060 <manishbhandari57290@gmail.com>
2024-10-02 17:43:17 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
+const todos = [{
+  id: 1,
+  title: "Todo 1",
+  description: "This is todo 1",
+  completed: false,
+}, {
+  id: 2,
+  title: "Todo 2",
+  description: "This is todo 2",
+  completed: false,
+}, {
+  id: 3,
+  title: "Todo 3",
+  description: "This is todo 3",
+  completed: false,
+
+}, {
+  id: 4,
+  title: "Todo 4",
+  description: "This is todo 4",
+  completed: false,
+}, {
+
+  id: 5,
+  title: "Todo 5",
+  description: "This is todo 5",
+  completed: false,
+}]
+
+app.get("/todo", (req, res) => {
+  const todo = todos.find(t => t.id == req.query.id);
+  res.json({
+    todo
+  })
+})
+
+app.get("/todos", (req, res) => {
+  const randomTodos = [];
+  for (let i = 0; i < 5; i++) {
+    if (Math.random() > 0.5) {
+      randomTodos.push(todos[i]);
+    }
+  }
+  res.json({
+    todos: randomTodos,
+  })
+});
 
 app.get("/sum", (req, res) => {
   const a = parseInt(req.query.a);
@@ -8,7 +58,7 @@ app.get("/sum", (req, res) => {
   res.send(sum.toString());
 });
 
-app.get("/interst", (req, res) => {
+app.get("/interest", (req, res) => {
   const principal = parseInt(req.query.principal);
   const rate = parseInt(req.query.rate);
   const time = parseInt(req.query.time);
@@ -17,7 +67,22 @@ app.get("/interst", (req, res) => {
   res.send({
     total: total,
     interest: interest,
-  });
+  })
+
 });
 
-app.listen = (8000)
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+app.get("/notifications", (req, res) => {
+  res.json({
+    network: getRandomNumber(10),
+    jobs: getRandomNumber(10),
+    messaging: getRandomNumber(10),
+    notifications: getRandomNumber(10)
+  })
+
+})
+
+app.listen(8080)
